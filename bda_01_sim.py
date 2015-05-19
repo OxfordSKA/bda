26c7c9fd9d3ea541adc865df583c465eb5217c5b
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2015-05-20 00:55:28 +0100

    updated sim script

--- bda_01_sim.py
+++ bda_01_sim.py
@@ -4,9 +4,14 @@
 Simulate a Measurement Set for use with BDA tests.
 """
 
+import os
+import numpy as np
+import time
+import subprocess
+
+
 def create_sky_model(sky_file, ra, dec, I):
     """Create an OSKAR sky model."""
-    import os
     if not os.path.dirname(sky_file):
         os.mkdir(os.path.dirname(sky_file))
     fh = open(sky_file, 'w')
@@ -17,31 +22,39 @@ def create_sky_model(sky_file, ra, dec, I):
 
 def dict_to_ini(settings_dict, ini):
     """Convert a dictionary of settings to and OSKAR settings ini file."""
-    import os
     ini_dir = os.path.dirname(ini)
     if not ini_dir == "" and not os.path.isdir(ini_dir):
         os.makedirs(ini_dir)
-    for group in sorted(settings_dict.keys()):
-        for key in sorted(settings_dict[group].keys()):
+    for group in sorted(settings_dict):
+        for key in sorted(settings_dict[group]):
             key_ = group+key
             value_ = settings_dict[group][key]
-            set(ini, key_, value_)
+            subprocess.call(["oskar_settings_set", "-q", ini,
+                            key_, str(value_)])
 
 
 def run_interferometer(ini, verbose=True):
     """Run the OSKAR interferometer simulator."""
-    from subprocess import call
     if verbose:
-        call(['oskar_sim_interferometer', ini])
+        subprocess.call(["oskar_sim_interferometer", ini])
     else:
-        call(['oskar_sim_interferometer', '-q', ini])
+        subprocess.call(["oskar_sim_interferometer", "-q", ini])
 
 
-def create_settings(ini_file, sky, ms):
+def create_settings(ini_file, sky, ms, ra0, dec0):
     """Create simulation settings file."""
-    import os
     if not os.path.isdir(os.path.dirname(ms)):
         os.mkdir(os.path.dirname(ms))
+    # --------------------------------------------------------------
+    dt = 1.6  # seconds
+    num_times = 100
+    freq = 700.0e6  # Hz
+    start_time = 57086.113194  # MJD UTC
+    lon0 = 21.442909  # deg
+    lat0 = -30.739475  # deg
+    telescope = 'models/SKA1_mid_combined_rmax_5.000_km.tm'
+    channel_bw = 0.0  # Hz
+    # --------------------------------------------------------------
     s = {}
     s['simulator/'] = {
         'max_sources_per_chunk': 1,
@@ -52,44 +65,42 @@ def create_settings(ini_file, sky, ms):
         'oskar_sky_model/file': sky
     }
     s['observation/'] = {
-        'start_frequency_hz': 700e6,
-        'start_time_utc': 57086.113194,
-        'length': 320.0,
-        'num_time_steps': 200,
-        'phase_centre_ra_deg': -90.3545848760,
-        'phase_centre_dec_deg': -8.5711239906
+        'start_frequency_hz': freq,
+        'start_time_utc': start_time,
+        'length': num_times*dt,
+        'num_time_steps': num_times,
+        'phase_centre_ra_deg': ra0,
+        'phase_centre_dec_deg': dec0
     }
     s['telescope/'] = {
-        'longitude_deg': 21.442909,
-        'latitude_deg': -30.739475,
-        'input_directory': 'models/SKA1_mid_combined_rmax_5.000_km.tm',
+        'longitude_deg': lon0,
+        'latitude_deg': lat0,
+        'input_directory': telescope,
         'pol_mode': 'Scalar',
         'station_type': 'Isotropic beam'
     }
     s['interferometer/'] = {
-        'time_average_sec': 1.6,
-        'channel_bandwidth_hz': 4000,
+        'time_average_sec': dt,
+        'channel_bandwidth_hz': channel_bw,
         'ms_filename': ms
     }
     dict_to_ini(s, ini_file)
     return s
 
 def oskar_sim():
-    import os
     # ---------------------------------------------
     ini = os.path.join('ini', 'test.ini')
-    ms = os.path.join('vis', 'test.ms')
+    ms = os.path.join('vis', 'vis_01.ms')
     sky = os.path.join('models', 'sky.osm')
-    ra0 = -90.3545848760
-    dec0 = -8.5711239906
+    ra0 = -90.3545848760  # deg
+    dec0 = -8.5711239906  # deg
     # ---------------------------------------------
     create_sky_model(sky, [ra0], [dec0+0.9], [1.0])
-    create_settings(ini, sky, ms)
+    create_settings(ini, sky, ms, ra0, dec0)
     run_interferometer(ini, verbose=False)
     return ms
 
 def main():
-    import time
     t0 = time.time()
     print '+ MS simulation...'
     ms = oskar_sim()
