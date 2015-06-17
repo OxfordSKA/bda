2be7655e377fe4bd5bd9f4bf3e194a70391e9ffd
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2015-06-17 22:59:12 +0100

    Added script to convert a Measurement Set to structured MATLAB .mat file. Updated simulation parameters.

--- util/ms_to_mat.py
+++ util/ms_to_mat.py
@@ -1,8 +1,42 @@
-#!/usr/bin/python
+# Run using:
+# casapy --nologger --nogui --log2term -c ms_to_mat <MS name>
 
-def main():
-    pass
+import os
+import sys
+import scipy.io
+import collections
 
+# Get MS name from command line.
+ms_name = sys.argv[-1]
 
-if __name__ == "__main__":
-    main()
+# Create top-level dictionary.
+d = collections.OrderedDict()
+k = os.path.splitext(os.path.basename(ms_name))[0]
+d[k] = collections.OrderedDict()
+
+# Get data from main table.
+tb.open(ms_name)
+col_names = tb.colnames()
+sub_tables = tb.getkeywords()
+for c in col_names:
+    try:
+        d[k][c] = tb.getcol(c)
+    except:
+        d[k][c] = []
+tb.close()
+
+# Get data from sub-tables.
+for s in sorted(sub_tables):
+    if str(sub_tables[s]).startswith('Table'):
+        tb.open(ms_name + '/' + s)
+        d[k][s] = collections.OrderedDict()
+        col_names = tb.colnames()
+        for c in col_names:
+            try:
+                d[k][s][c] = tb.getcol(c)
+            except:
+                d[k][s][c] = []
+        tb.close()
+
+# Write data to MATLAB file.
+scipy.io.savemat(ms_name + '.mat', d, do_compression=True)
