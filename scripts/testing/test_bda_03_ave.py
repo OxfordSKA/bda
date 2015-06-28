fixed bug in averaging the time columns due to not resetting the average time accumulation buffer

scripts/testing/test_bda_03_ave.py
@@ -6,23 +6,35 @@ import pickle
 import progressbar
 import matplotlib.pyplot as plt
 
+
 def load_ms(ms):
-    dtype = [('a1','i4'), ('a2', 'i4'), ('data', 'c16'), ('t', 'f8')]
+    dtype = [('uu', 'f8'), ('vv', 'f8'), ('ww', 'f8'),
+             ('time', 'f8'), ('time_c', 'f8'),
+             ('a1', 'i4'), ('a2', 'i4'), ('data', 'c16'), ('t', 'f8')]
     tb.open(ms)
     num_rows = tb.nrows()
     values = np.zeros((num_rows,), dtype=dtype)
     values['data'] = np.squeeze(tb.getcol('DATA'))
     values['a1'] = np.squeeze(tb.getcol('ANTENNA1'))
     values['a2'] = np.squeeze(tb.getcol('ANTENNA2'))
+    uvw = tb.getcol('UVW')
+    values['uu'] = uvw[0, :]
+    values['vv'] = uvw[1, :]
+    values['ww'] = uvw[2, :]
     time = tb.getcol('TIME')
+    values['time'] = time
     time -= time[0]
     values['t'] = time
+    time_c = tb.getcol('TIME_CENTROID')
+    values['time_c'] = time_c
     tb.close()
     return values
 
 
 def load_ms_2(ms):
-    dtype = [('a1','i4'), ('a2', 'i4'), ('data', 'c16'), ('t', 'f8'),
+    dtype = [('uu', 'f8'), ('vv', 'f8'), ('ww', 'f8'),
+             ('time', 'f8'), ('time_c', 'f8'),
+             ('a1', 'i4'), ('a2', 'i4'), ('data', 'c16'), ('t', 'f8'),
              ('model_data', 'c16'), ('corrected_data', 'c16')]
     tb.open(ms)
     num_rows = tb.nrows()
@@ -32,17 +44,49 @@ def load_ms_2(ms):
     values['corrected_data'] = np.squeeze(tb.getcol('CORRECTED_DATA'))
     values['a1'] = np.squeeze(tb.getcol('ANTENNA1'))
     values['a2'] = np.squeeze(tb.getcol('ANTENNA2'))
+    uvw = tb.getcol('UVW')
+    print uvw.shape
+    #  values['uu']
     time = tb.getcol('TIME')
+    values['time'] = time
     time -= time[0]
     values['t'] = time
+    time_c = tb.getcol('TIME_CENTROID')
+    values['time_c'] = time_c
     tb.close()
     return values
 
-
-test_model = False
+test_model = True
+test_model_ave = True
 test_corrupted = True
+
 if test_model:
     print 'Model:'
+    default_ms = os.path.join('out_default', 'vis', 'model.ms')
+    new_ms = os.path.join('out_new', 'vis', 'model.ms')
+
+    default = load_ms(default_ms)
+    new = load_ms(new_ms)
+
+    print '*' * 60
+    print default.shape
+    print new.shape
+    print '*' * 60
+
+    print 'Diffs:'
+    print '  DATA  :', np.max(np.abs(default['data'] - new['data']))
+    print '  TIME  :', np.max(np.abs(default['time'] - new['time']))
+    print '  TIMEC :', np.max(np.abs(default['time_c'] - new['time_c']))
+    print '  UU    :', np.max(np.abs(default['uu'] - new['uu']))
+    print '  VV    :', np.max(np.abs(default['vv'] - new['vv']))
+    print '  WW    :', np.max(np.abs(default['ww'] - new['ww']))
+    print '  ANT1  :', np.max(np.abs(default['a1'] - new['a1']))
+    print '  ANT2  :', np.max(np.abs(default['a2'] - new['a2']))
+    print ''
+
+
+if test_model_ave:
+    print 'Model ave:'
     default_ms = os.path.join('out_default', 'vis', 'model_mstransform.ms')
     new_ms = os.path.join('out_new', 'vis', 'model_bda.ms')
 
@@ -54,8 +98,26 @@ if test_model:
     print new.shape
     print '*' * 60
 
-    print 'diffs:'
-    print 'data', np.max(default['data'] - new['data'])
+    # diff_time = np.abs(default['time'] - new['time'])
+    # plt.plot(diff_time[1250:1300], '+')
+    # plt.show()
+    # diff_data = np.abs(default['data'] - new['data'])
+    # print len(diff_time[diff_time > 1.0])
+    # print default['time'][1000]
+    # print new['time'][1000]
+    # plt.plot(diff_data[0:10000], '+')
+    # plt.show()
+
+    print 'Diffs:'
+    print '  DATA  :', np.max(np.abs(default['data'] - new['data']))
+    print '  TIME  :', np.max(np.abs(default['time'] - new['time']))
+    print '  TIMEC :', np.max(np.abs(default['time_c'] - new['time_c']))
+    print '  T     :', np.max(np.abs(default['t'] - new['t']))
+    print '  UU    :', np.max(np.abs(default['uu'] - new['uu']))
+    print '  VV    :', np.max(np.abs(default['vv'] - new['vv']))
+    print '  WW    :', np.max(np.abs(default['ww'] - new['ww']))
+    print '  ANT1  :', np.max(np.abs(default['a1'] - new['a1']))
+    print '  ANT2  :', np.max(np.abs(default['a2'] - new['a2']))
     print ''
 
 if test_corrupted:
@@ -71,8 +133,15 @@ if test_corrupted:
     print new.shape
     print '*' * 60
 
-    print 'diffs:'
-    print 'data           :', np.max(np.abs(default['data'] - new['data']))
-    print 'model_data     :', np.max(np.abs(default['model_data'] - new['model_data']))
-    print 'corrected_data :', np.max(np.abs(default['corrected_data'] - new['corrected_data']))
+    print 'Diffs:'
+    print '  DATA  :', np.max(np.abs(default['data'] - new['data']))
+    print '  TIME  :', np.max(np.abs(default['time'] - new['time']))
+    print '  TIMEC :', np.max(np.abs(default['time_c'] - new['time_c']))
+    print '  T     :', np.max(np.abs(default['t'] - new['t']))
+    print '  UU    :', np.max(np.abs(default['uu'] - new['uu']))
+    print '  VV    :', np.max(np.abs(default['vv'] - new['vv']))
+    print '  WW    :', np.max(np.abs(default['ww'] - new['ww']))
+    print '  ANT1  :', np.max(np.abs(default['a1'] - new['a1']))
+    print '  ANT2  :', np.max(np.abs(default['a2'] - new['a2']))
+    print ''
 