bc7284dcc7353cce2df7e34ff89ed3eed0c320c1
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2016-02-03 18:08:30 +0000

    minor tweaks to stefcal module

--- pybda/stefcal.py
+++ pybda/stefcal.py
@@ -1,15 +1,22 @@
 # coding=utf-8
+"""
+Python implementation of StefCal calibration algorithm.
+"""
 
-import numpy as num
-import numpy.linalg as LA
+
+import numpy
+import numpy.linalg
 
 
-def stefcal1(a, b, tol, niter, gstart=None):
+def stefcal1(a, b, tol=1.0e-8, niter=50, gstart=None):
     """
-    Python implementation of StefCal calibration alogorithm
+    Python implementation of StefCal calibration algorithm.
 
     Description:
-        Minimize  || a - G' * b * G ||_F where G = diag(g)
+        Minimize
+            ``|| a - G' * b * G ||_F``
+        where
+            ``G = diag(g)``
 
     Args:
         a (array_like): Observed visibilities (full matrix)
@@ -21,18 +28,18 @@ def stefcal1(a, b, tol, niter, gstart=None):
                              at a previous time are known.
 
     Returns:
-        g[], nit, dg
-        g (array_like): computed gains, g(i, 1) corresponds to the ith receiver.
+        tuple of g, nit, dg
+
+        g (array_like): computed gains, g[i] corresponds to the ith receiver.
         nit (int): number of iterations required.
         dg (float): convergence achieved.
     """
-
     n = a.shape[0]
 
     if gstart is not None:
         g = gstart
     else:
-        g = num.ones(n, num.complex128)
+        g = numpy.ones(n, numpy.complex128)
 
     dg = 1.0e30
     nit = niter
@@ -40,36 +47,32 @@ def stefcal1(a, b, tol, niter, gstart=None):
     f0 = 1 - omega
     f1 = omega
 
-    for iter in range(niter):
-        gold = num.copy(g)
+    for i in range(niter):
+        g_old = numpy.copy(g)
         nit = niter
-        if iter < 2:
+        if i < 2:
             for j in range(n):
-                z = num.conj(gold)*b[:, j]
-                w = num.dot(num.conj(z), z)
-                t = num.dot(num.conj(z), a[:, j])
-                t = t/w
-                g[j] = t
-            dg = LA.norm(g-gold) / LA.norm(g)
+                z = numpy.conj(g_old) * b[:, j]
+                g[j] = numpy.dot(numpy.conj(z), a[:, j]) / \
+                    numpy.dot(numpy.conj(z), z)
+            dg = numpy.linalg.norm(g - g_old) / numpy.linalg.norm(g)
             if dg <= tol:
-                nit = iter
+                nit = i
                 break
         else:
             for j in range(n):
-                z = num.conj(gold)*b[:, j]
-                w = num.dot(num.conj(z), z)
-                t = num.dot(num.conj(z), a[:, j])
-                t = t/w
-                g[j] = t
-            if iter % 2 == 1 and iter > 0:
-                dg = LA.norm(g - gold) / LA.norm(g)
+                z = numpy.conj(g_old) * b[:, j]
+                g[j] = numpy.dot(numpy.conj(z), a[:, j]) / \
+                    numpy.dot(numpy.conj(z), z)
+            if i % 2 == 1 and i > 0:
+                dg = numpy.linalg.norm(g - g_old) / numpy.linalg.norm(g)
                 if dg <= tol:
-                    nit = iter
+                    nit = i
                     break
                 else:
-                    g = f0 * g + f1 * gold
+                    g = f0 * g + f1 * g_old
 
-    p = num.conj(g[0]) / num.abs(g[0])
+    p = numpy.conj(g[0]) / numpy.abs(g[0])
     g = p * g
 
     return g, nit, dg
